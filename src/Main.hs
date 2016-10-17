3be9547cc5ca6424b7a8346bfb7f1ed1bc094f38
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2016-10-18 01:12:32 +0500

    skeleton of rewriting in haskell

---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+-- Author: Viacheslav Lotsmanov
+-- License: GPLv3 https://raw.githubusercontent.com/unclechu/xlib-keys-hack/master/LICENSE
+
+module Main where
+
+import Graphics.X11.Xlib
+
+main :: IO ()
+main = putStrLn "Test"
